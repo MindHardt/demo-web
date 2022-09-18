dff6668a2543b0af901340c1f2651909066358c5
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-09-18 22:09:34 +0500

    Added bootstrap

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,13 +3,15 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class PostService {
     public Post[] getAllPosts() {
         return new Post[ ]{
-            new Post("Post №1"),
-            new Post("Lorem Ipsum Dolorem Sit Amet"),
-            new Post("Hocus Pocus Fidibus")
+            new Post("Post №1", new Date()),
+            new Post("Lorem Ipsum Dolorem Sit Amet", new Date()),
+            new Post("Hocus Pocus Fidibus", new Date())
         };
     }
 }
